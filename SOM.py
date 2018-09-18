f0076045ee1e8d648267f0dd060f5df1780a81b8
DevHunter <32465888+DevHunterYZ@users.noreply.github.com>
2018-09-18 18:06:28 +0300

    Update SOM.py

--- SOM.py
+++ SOM.py
@@ -3,17 +3,17 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
-#Verisetini çağıralım.
+# Verisetini çağıralım.
 dataset=pd.read_csv('C:/Users/user/Desktop/Credit_Card_Applications.csv')
 X=dataset.iloc[:,:-1].values
-X=dataset.iloc[:,-1].values
+y=dataset.iloc[:,-1].values
 
-#Feature scaling
+# Feature scaling
 from sklearn.preprocessing import MinMaxScaler
 sc=MinMaxScaler(feature_range=(0,1))
 sc.fit_transform(X)
 
-#SOM eğitimi
+# SOM eğitimi
 from minisom import MiniSom
 som=MiniSom(x=10, y=10, input_len=15,sigma=1.0,learning_rate=0.5)
 som.random_weights_init(X)
